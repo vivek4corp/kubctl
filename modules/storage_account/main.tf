cc8ed8bf6b29fdca69c0dfce80a83785d50b3aa9
vivek4corp <137039398+vivek4corp@users.noreply.github.com>
2026-07-14 09:18:22 +0530

    Create main.tf

---
+++ modules/storage_account/main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_storage_account" "this" {
+  for_each = var.storage_accounts
+
+  name                     = each.key
+  resource_group_name      = each.value.resource_group_name
+  location                 = each.value.location
+  account_tier             = each.value.account_tier
+  account_replication_type = each.value.account_replication_type
+  kind                     = each.value.kind
+
+  tags = each.value.tags
+}
+
+output "storage_account_names" {
+  value = { for k, v in azurerm_storage_account.this : k => v.name }
+}

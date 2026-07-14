176cfe6358fe1aac6ad027da2ec3788d5fcbff82
vivek4corp <137039398+vivek4corp@users.noreply.github.com>
2026-07-14 09:10:57 +0530

    Update main.tf to include storage accounts module
    Commented out the ACR module and added a storage accounts module.

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,7 +2,7 @@ module "resource_groups" {
   source          = "../../modules/resource_group"
   resource_groups = var.infra_config.resource_groups
 }
-
+/*
 module "acr" {
   source = "../../modules/acr"
   container_registries = {
@@ -28,3 +28,20 @@ module "aks" {
     }
   }
 }
+*/
+
+module "storage_accounts" {
+  source = "../../modules/storage_account"
+
+  storage_accounts = {
+    for k, v in var.infra_config.storage_accounts : k => {
+      resource_group_name      = module.resource_groups.resource_group_names[v.rg_key]
+      location                 = var.infra_config.resource_groups[v.rg_key].location
+      account_tier             = v.account_tier
+      account_replication_type = v.account_replication_type
+      kind                     = v.kind
+      tags                     = v.tags
+    }
+  }
+}
+

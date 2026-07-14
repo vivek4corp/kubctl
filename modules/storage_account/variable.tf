a551221997198074d67fb58a8fab3196146b5d0b
vivek4corp <137039398+vivek4corp@users.noreply.github.com>
2026-07-14 09:18:04 +0530

    Create variable.tf

---
+++ modules/storage_account/variable.tf
@@ -0,0 +1,11 @@
+variable "storage_accounts" {
+  description = "Map of storage accounts to create"
+  type = map(object({
+    resource_group_name      = string
+    location                 = string
+    account_tier             = string
+    account_replication_type = string
+    kind                     = string
+    tags                     = optional(map(string), {})
+  }))
+}

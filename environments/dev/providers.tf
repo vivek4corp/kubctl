5d9f6a7787f5a56f5fb791e5d188814c32c9bb1b
vivek4corp <137039398+vivek4corp@users.noreply.github.com>
2026-07-20 15:19:28 +0530

    Update providers.tf

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -6,10 +6,6 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "rg-tfstate"
-    storage_account_name = "stterraformstate001110"
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
   }
 }
 
